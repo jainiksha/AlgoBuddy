b06f61ae08dc27125ce57f04d6b2399032a8a04c
TanCodeX <tanmaypatwary@gmail.com>
2026-05-29 13:15:13 +0530

    feat(visualizer): append Comparison Mode to sorting lists

--- app/visualizer/searching/binarysearch/animation.jsx
+++ app/visualizer/searching/binarysearch/animation.jsx
@@ -155,7 +155,7 @@ const BinarySearch = () => {
         }, delay * 0.6);
       }
     }, delay);
-  }, [speedRef]);
+  }, [isPausedRef, speedRef]);
 
   const handleGo = (e) => {
     e.preventDefault();
